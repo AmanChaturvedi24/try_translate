84da15125d2b7366c00e14c1c79c50537ffe2db8
Aman Chaturvedi <73428540+AmanChaturvedi24@users.noreply.github.com>
2022-09-13 07:53:06 +0530

    final update

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ var input_text= document.querySelector("#input-text");
 var btn= document.querySelector(".btn-translate");
 var output_text= document.querySelector("#output-text");
 
-var url="https://api.funtranslations.com/translate/minion.json";
+var url="https://api.funtranslations.com/translate/pirate.json";
 
 function GetItTranslate(text) {
-    return url+"?"+"text"+text;
+    return url+"?"+"text="+text;
 }
 
 btn.addEventListener("click",function() {
     console.log("hello clicked")
     fetch(GetItTranslate(input_text.value))
     .then(response =>response.json())
-    .then(json => {console.log(json.contents.translated)})
-    // console.log(done);
+    .then(json => {output_text.innerText=json.contents.translated})
 });
  
 
